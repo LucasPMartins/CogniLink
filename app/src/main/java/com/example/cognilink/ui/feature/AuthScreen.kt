560deef4c40ae73fe31959b789ba36ba46336a25
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2026-04-08 13:18:07 -0300

    Init commit

---
+++ app/src/main/java/com/example/cognilink/ui/feature/AuthScreen.kt
@@ -0,0 +1,24 @@
+package com.example.cognilink.ui.feature
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cognilink.ui.theme.CogniLinkTheme
+
+@Composable
+fun AuthScreen() {
+    
+}
+
+@Composable
+fun AuthContent() {
+    
+}
+
+@Preview
+@Composable
+private fun AuthContentPreview() {
+    CogniLinkTheme{
+        AuthContent()
+    }
+}
---
+++ app/src/main/java/com/example/cognilink/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.cognilink.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkNavyBlue = Color(0xFF1A237E)
+
+val BlueGray = Color(0xFF71749a)
+
+val VividCyan = Color(0xFF22D3EE)
+
+val OffWhite = Color(0xFFF5F5F5)
+
+val DarkGray = Color(0xFF454652)
+
+val LightGray = Color(0xFF767683)
+
+val Black = Color(0xFF000000)
